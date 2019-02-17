2f62bf6a71c92aa60e21a1794babdba26e812b0d
Niko Kivelä <niko@tovrleaf.com>
2019-02-17 22:07:13 +0200

    Update stock with given symbol

--- stockapi/cli/stock.py
+++ stockapi/cli/stock.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import sys
 
 @click.group('stock')
 def cli():
@@ -20,3 +21,25 @@ def add_new_share(symbol):
     }
   )
   click.echo('Adding %s as new share.' % symbol)
+
+@cli.command('refresh')
+@click.option('-s', '--symbol',
+              required=True,
+              help='Identifier for share used in Nasdaq')
+def update_existing_share(symbol):
+  table = boto3.resource('dynamodb').Table('mystock_share')
+  response = table.get_item(
+    Key={
+      'symbol': symbol
+    }
+  )
+  if 'Item' not in response:
+    click.secho('Cannot find a share with symbol %s.' % symbol, fg='red')
+    sys.exit(128)
+
+  item = response['Item']
+  item['price'] = 1
+
+  table.put_item(Item=item)
+
+  click.echo('Updating %s as existing share.' % symbol)
