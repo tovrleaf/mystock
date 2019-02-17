b5f9cd60a5785dba318472ec08a58d298d023f09
Niko Kivelä <niko@tovrleaf.com>
2019-02-17 23:17:47 +0200

    Fetch share info from api

--- stockapi/services/stock_service.py
+++ stockapi/services/stock_service.py
@@ -1,13 +1,17 @@
 import boto3
+from decimal import Decimal
+import requests
 from stockapi.exceptions import StockNotFoundException
 
 
 class StockService(object):
 
+    table_name = 'mystock_share'
+
     def insert_share(self, symbol):
         dynamodb = boto3.client('dynamodb')
         dynamodb.put_item(
-            TableName='mystock_share',
+            TableName=self.table_name,
             Item={
                 'symbol': {
                     'S': symbol
@@ -16,7 +20,7 @@ class StockService(object):
         )
 
     def update_share(self, symbol):
-        table = boto3.resource('dynamodb').Table('mystock_share')
+        table = boto3.resource('dynamodb').Table(self.table_name)
         response = table.get_item(
             Key={
                 'symbol': symbol
@@ -27,6 +31,41 @@ class StockService(object):
               'Cannot find a share with symbol %s.' % symbol)
 
         item = response['Item']
-        item['price'] = 1
+
+        self.__update_item_by_name(symbol, item)
 
         table.put_item(Item=item)
+
+    def __update_item_by_name(self, name, item):
+
+        def get_movement_for_share(name):
+            url = ('https://www.kauppalehti.fi/backend/'
+                   'stock;cache=false;endpoint=stock/'
+                   'intraday/graph/{}/LATEST')
+            r = requests.get(url.format(name))
+            r.raise_for_status()
+            return r.json()['movement']
+
+        def get_balance_for_share(name, kind):
+            url = ('https://www.kauppalehti.fi/backend/'
+                   'stock;cache=false;endpoint=balance/'
+                   '{}/{}/5')
+            r = requests.get(url.format(kind, name))
+            r.raise_for_status()
+            return r.json()
+
+        valuation = get_balance_for_share(name, 'valuation')
+        interimreports = get_balance_for_share(name, 'interimreports')
+        share_price = get_movement_for_share(name)[0][1]
+
+        def f2d(v):
+            """Float to Decimal"""
+            return Decimal(str(v))
+
+        # Kurssi
+        item['price'] = f2d(share_price)
+        # Osinkotuotto
+        item['dividendYield'] = f2d(valuation['dividendYield'])
+        # Tulos/Pääoma
+        item['returnOnEquity'] = f2d(
+            interimreports['adjustedReturnOnEquity12M'])
